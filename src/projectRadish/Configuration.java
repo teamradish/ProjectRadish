9726e3976db07db60cca71a1ed697c4fd48591ba
tdeeb <thomasdeeb1@gmail.com>
2018-05-09 00:04:36 -0700

    Imported the jackson library to save/load the Config JSON directly to/from a Java class
    -Added a Config class
    -Added a private static Config field in Configuration for encapsulation
    -Made all getters and setters in Configuration reference the Config instance's fields
    -Condensed code

--- src/projectRadish/Configuration.java
+++ src/projectRadish/Configuration.java
@@ -1,11 +1,19 @@
 package projectRadish;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.internal.objects.Global;
+import jdk.nashorn.internal.parser.JSONParser;
+import jdk.nashorn.internal.runtime.Context;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -16,103 +24,87 @@ public final class Configuration
     // Private Constructor to prevent instantiation
     private Configuration() {}
 
-    private static String BotToken;
-    private static Set<String> RadishAdmin  = new HashSet<>();
-    private static Set<String> TPEAdmin  = new HashSet<>();
-    private static String CurrentGame;
-    private static String CurrentDoc;
-    private static HashMap<String, String> Docs = new HashMap<>();
-
+    /**
+     * The reference to the config object used.
+     */
+    private static Config config = null;
 
     public static Set<String> getRadishAdmin() {
-        return RadishAdmin;
+        return config.RadishAdmin;
     }
 
     public static Set<String> getTPEAdmin() {
-        return TPEAdmin;
+        return config.TPEAdmin;
     }
 
     public static String getBotToken() {
-        return BotToken;
+        return config.BotToken;
     }
 
     public static String getCurrentDoc() {
-        return CurrentDoc;
+        return config.CurrentDoc;
     }
 
     public static void setCurrentDoc(String currentDoc) {
-        Configuration.CurrentDoc = currentDoc;
+        config.CurrentDoc = currentDoc;
     }
 
     public static String getCurrentGame() {
-        return CurrentGame;
+        return config.CurrentGame;
     }
 
     public static void setCurrentGame(String currentGame) {
-        CurrentGame = currentGame;
+        config.CurrentGame = currentGame;
     }
 
     public static HashMap<String, String> getDocs() {
-        return Docs;
+        return config.Docs;
     }
 
     public static void loadConfiguration()
     {
-        try {
-            JSONObject configJSON = new JSONObject(new String(Files.readAllBytes(Paths.get("config.json"))));
-            BotToken = configJSON.getString("BotToken");
-            CurrentDoc = configJSON.getString("CurrentDoc");
-            CurrentGame = configJSON.getString("CurrentGame");
-            JSONArray radishAdminJSON = configJSON.getJSONArray("RadishAdmin");
-            for (Object key : radishAdminJSON)
-                RadishAdmin.add((String) key);
-            JSONArray tpeAdminJSON = configJSON.getJSONArray("TPEAdmin");
-            for (Object key : tpeAdminJSON)
-                TPEAdmin.add((String) key);
-            JSONObject docsJSON = configJSON.getJSONObject("Docs");
-            Iterator<String> keysItr = docsJSON.keys();
-            while (keysItr.hasNext()) {
-                String key = keysItr.next();
-                Object value = docsJSON.get(key);
-                Docs.put(key, (String)value);
-            }
-        } catch (IOException e) {
-            RadishAdmin.add("<RadishAdmin #1 ID>");
-            RadishAdmin.add("<RadishAdmin #2 ID>");
-            TPEAdmin.add("<TPEAdmin #1 ID>");
-            TPEAdmin.add("<TPEAdmin #2 ID>");
-            Docs.put("<DOC NAME #1>", "<DOC URL #1>");
-            Docs.put("<DOC NAME #2>", "<DOC URL #2>");
-            JSONObject configJSON = new JSONObject();
-            configJSON.put("BotToken","<INSERT BOT KEY HERE>");
-            configJSON.put("RadishAdmin", RadishAdmin);
-            configJSON.put("TPEAdmin", TPEAdmin);
-            configJSON.put("CurrentGame", "<Current game name>");
-            configJSON.put("CurrentDoc", "<Current document URL>");
-            configJSON.put("Docs", Docs);
+        ObjectMapper mapper = new ObjectMapper();
 
-            try {
-                Files.write(Paths.get("config.json"), configJSON.toString(4).getBytes());
-                System.out.println("config.json not found. A new file was created.");
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                System.out.println("config.json not found. Error creating new file.");
-            }
+        //Don't fail on unknown properties
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        try
+        {
+            //Convert from JSON to our object
+            config = mapper.readValue(new String(Files.readAllBytes(Paths.get("config.json"))), Config.class);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+
+            //Handle not being able to read or parse JSON
+            config = new Config();
+            config.RadishAdmin.add("<RadishAdmin #1 ID>");
+            config.RadishAdmin.add("<RadishAdmin #2 ID>");
+            config.TPEAdmin.add("<TPEAdmin #1 ID>");
+            config.TPEAdmin.add("<TPEAdmin #2 ID>");
+            config.Docs.put("<DOC NAME #1>", "<DOC URL #1>");
+            config.Docs.put("<DOC NAME #2>", "<DOC URL #2>");
+
+            //If it doesn't exist, create and save the new file
+            //Kimimaru: Do we really want to do this? It might be a problem if it couldn't
+            //read the JSON for some reason but the file still has all the info
+            saveConfiguration();
         }
     }
 
     public static void saveConfiguration()
     {
-        try {
-            JSONObject configJSON = new JSONObject();
-            configJSON.put("BotToken", BotToken);
-            configJSON.put("RadishAdmin", RadishAdmin);
-            configJSON.put("TPEAdmin", TPEAdmin);
-            configJSON.put("CurrentDoc", CurrentDoc);
-            configJSON.put("CurrentGame", CurrentGame);
-            configJSON.put("Docs", Docs);
-            Files.write(Paths.get("config.json"), configJSON.toString(4).getBytes());
-        } catch (IOException e) {
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+
+            //Write JSON as pretty so we can read it
+            String data = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(config);
+            Files.write(Paths.get("config.json"), data.getBytes());
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
             System.out.println("Couldn't save config.json.");
         }
