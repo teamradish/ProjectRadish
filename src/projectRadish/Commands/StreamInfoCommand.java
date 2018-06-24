09e254cc3ce5d9cf6179bbb5f614163a0b53edea
tdeeb <thomasdeeb1@gmail.com>
2018-06-23 18:08:37 -0700

    Fixed stream uptime by specifying the UTC time zone, and displayed the stream's game name instead of the game ID

--- src/projectRadish/Commands/StreamInfoCommand.java
+++ src/projectRadish/Commands/StreamInfoCommand.java
@@ -16,6 +16,7 @@ public class StreamInfoCommand extends BaseCommand
 {
     private static final String CLIENT_ID = "rrixp6h00ku9ic34l1mbvilkl7qi8c";
     private static final String TWITCH_STREAM =  "https://api.twitch.tv/helix/streams?user_login=twitchplays_everything";
+    private static final String GAME_ENDPOINT = "https://api.twitch.tv/helix/games?id=";
 
     private ObjectMapper objMapper = new ObjectMapper();
 
@@ -46,18 +47,25 @@ public class StreamInfoCommand extends BaseCommand
             StreamResponse response = streamInfo.data[0];
 
             String uptime = "N/A";
+            String gameName = response.game_id;
 
             try
             {
-                //Get the uptime from the difference of the current time and the stream's time
-                LocalDateTime now = LocalDateTime.now();
+                //Get the uptime from the difference of the current UTC time and the stream's time (which is in UTC)
+                LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+
+                System.out.println(now.toString());
 
                 //Parse stream time from the string given
                 LocalDate streamDate = LocalDate.parse(response.started_at.substring(0, 10));
                 LocalTime streamTime = LocalTime.parse(response.started_at.substring(11, response.started_at.length() - 1));
                 LocalDateTime streamDateTime = LocalDateTime.of(streamDate, streamTime);
 
+                System.out.println(streamDateTime.toString());
+
                 Duration duration = Duration.between(now, streamDateTime);
+
+                System.out.println(duration.toString());
 
                 final long secondsPerMinute = 60;
                 final long secondsPerHour = 60 * 60;
@@ -71,17 +79,21 @@ public class StreamInfoCommand extends BaseCommand
                 int seconds = (int) (totalSeconds % secondsPerMinute);
 
                 uptime = days + " days, " + hours + " hrs, " + minutes + " min, " + seconds + "sec";
+
+                GameData gameData = GetGameData(response.game_id);
+                if (gameData != null && gameData.data != null && gameData.data.length > 0)
+                    gameName = gameData.data[0].name;
             }
             catch (Exception e)
             {
                 System.out.println(e.getMessage());
             }
 
-            infoString = "Status: " + response.type + "\n"
-            + "Title: " + response.title + "\n"
-            + "GameID: " + response.game_id + "\n"
-            + "Uptime: " + uptime + "\n"
-            + "Viewers: " + response.viewer_count;
+            infoString = "**Status:** " + response.type + "\n"
+            + "**Title:** " + response.title + "\n"
+            + "**Game:** " + gameName + "\n"
+            + "**Uptime:** " + uptime + "\n"
+            + "**Viewers:** " + response.viewer_count;
         }
 
         event.getChannel().sendMessage(infoString).queue();
@@ -107,6 +119,34 @@ public class StreamInfoCommand extends BaseCommand
             StreamData streamData = objMapper.readValue(inputLine, StreamData.class);
 
             return streamData;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GameData GetGameData(String gameID)
+    {
+        try
+        {
+            //URL to Twitch stream information
+            URL url = new URL(GAME_ENDPOINT + gameID);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            //Add the client ID as the header
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Client-ID", CLIENT_ID);
+
+            //Read information
+            BufferedReader br = new BufferedReader( new InputStreamReader( conn.getInputStream() ));
+            String inputLine = br.readLine();
+            br.close();
+
+            GameData gameData = objMapper.readValue(inputLine, GameData.class);
+
+            return gameData;
         }
         catch (IOException e)
         {
